86181fe9297efa7712de83343d76de052687e8e1
Ankur Maurya <100197611+ankurkmaurya@users.noreply.github.com>
2022-10-27 18:08:13 +0530

    Java Doc Updated

--- Creational Patterns/CreationalPatterns/src/main/java/builder/Builder.java
+++ Creational Patterns/CreationalPatterns/src/main/java/builder/Builder.java
@@ -5,33 +5,33 @@ package builder;
 //@formatter:off
 /**
 * 
-* Builder Design Pattern
+* <h1>Builder Design Pattern</h1>
 * 
 * Builder is a creational design pattern that lets you construct complex objects step by step. 
 * The pattern allows you to produce different types and representations of an object using the same construction code.
 * 
 * 
-* Structure : 
+* <h3>Structure : </h3>
 * The Reader encapsulates the parsing of the common input. 
 * The Builder hierarchy makes possible the polymorphic creation of many peculiar representations or targets.
 * 
 * 
-* Points : 
-* -> Use the Builder pattern when you want your code to be able to create different representations of some product.
-* -> The Builder pattern can be applied when construction of various representations of the product involves 
-*    similar steps that differ only in the details.
-* -> The Builder pattern can be recognized in a class, which has a single creation method and several methods to configure the resulting object. 
-* -> Builder methods often support chaining (for example, someBuilder.setValueA(1).setValueB(2).create()).
-* -> The only big difference between the builder pattern and the abstract factory pattern is that builder 
-*    provides us more control over the object creation process.
-* 
-* 
+* <h3>Points : </h3>
+* <ul>
+*  <li>Use the Builder pattern when you want your code to be able to create different representations of some product.</li>
+*  <li>The Builder pattern can be applied when construction of various representations of the product involves 
+*    similar steps that differ only in the details.</li>
+*  <li>The Builder pattern can be recognized in a class, which has a single creation method and several methods to configure the resulting object.</li>
+*  <li>Builder methods often support chaining (for example, someBuilder.setValueA(1).setValueB(2).create()).</li>
+*  <li>The only big difference between the builder pattern and the abstract factory pattern is that builder 
+*    provides us more control over the object creation process.</li>
+* </ul>
 *     
-* 
-* Builder Design Pattern examples in JDK
-*	- java.lang.StringBuilder#append() (unsynchronized)
-*   - java.lang.StringBuffer#append() (synchronized)
-* 
+* <h3>Builder Design Pattern examples in JDK</h3>
+* <ul>
+*  <li>java.lang.StringBuilder#append() (unsynchronized)</li>
+*  <li>java.lang.StringBuffer#append() (synchronized)</li>
+* </ul>
 * 
 * @author Ankur Maurya
 * 
--- Creational Patterns/CreationalPatterns/src/main/java/factorymethod/FactoryMethod.java
+++ Creational Patterns/CreationalPatterns/src/main/java/factorymethod/FactoryMethod.java
@@ -8,39 +8,41 @@ import abstractfactory.shape.ShapeType;
 //@formatter:off
 /**
 * 
-* Factory Method Design Pattern
+* <h1>Factory Method Design Pattern</h1>
 * 
-* Structure : 
+* <h3>Structure : </h3>
 * The factory design pattern is used when we have a superclass with multiple sub-classes and based on input, 
 * we need to return one of the sub-class.
 * 
-* Checklist : 
-* -> If you have an inheritance hierarchy that exercises polymorphism, consider adding a polymorphic 
-*    creation capability by defining a static factory method in the base class.
-* -> Design the arguments to the factory method. What qualities or characteristics are necessary and 
-*    sufficient to identify the correct derived class to instantiate?
-* -> Consider designing an internal "object pool" that will allow objects to be reused instead of created from scratch.
-* -> Consider making all constructors private or protected.
+* <h3>Checklist : </h3>
+* <ul>
+*  <li>If you have an inheritance hierarchy that exercises polymorphism, consider adding a polymorphic 
+*    creation capability by defining a static factory method in the base class.</li>
+*  <li>Design the arguments to the factory method. What qualities or characteristics are necessary and 
+*    sufficient to identify the correct derived class to instantiate?</li>
+*  <li>Consider designing an internal "object pool" that will allow objects to be reused instead of created from scratch.</li>
+*  <li>Consider making all constructors private or protected.</li>
+* </ul>
+* 
+* <h3>Points : </h3>
+* <ul>
+*  <li>This pattern takes out the responsibility of the instantiation of a class from the client program to the factory class.</li>
+*  <li>Factory Methods are usually called within Template Methods.</li>
+*  <li>Factory Method: creation through inheritance. Prototype: creation through delegation.</li>
+*  <li>The advantage of a Factory Method is that it can return the same instance multiple times, 
+*       or can return a subclass rather than an object of that exact type.</li>
+*  <li>Super class in factory design pattern can be an interface, abstract class or a normal java class.</li>
+*  <li>Factory pattern provides abstraction between implementation and client classes through inheritance.</li>
+*  <li>Factory pattern removes the instantiation of actual implementation classes from client code.</li>
+*      Factory pattern makes our code more robust, less coupled and easy to extend.</li>
+* </ul>
 * 
 * 
-* Points : 
-* -> This pattern takes out the responsibility of the instantiation of a class from the client program to the factory class.
-* -> Factory Methods are usually called within Template Methods.
-* -> Factory Method: creation through inheritance. Prototype: creation through delegation.
-* -> The advantage of a Factory Method is that it can return the same instance multiple times, 
-*    or can return a subclass rather than an object of that exact type.
-* -> Super class in factory design pattern can be an interface, abstract class or a normal java class.
-* -> Factory pattern provides abstraction between implementation and client classes through inheritance.
-* -> Factory pattern removes the instantiation of actual implementation classes from client code. 
-*    Factory pattern makes our code more robust, less coupled and easy to extend.
-* 
-* 
-* Factory Method Design Pattern examples in JDK
-*	- java.util.Calendar, ResourceBundle and NumberFormat getInstance() methods uses Factory pattern.
-*   - valueOf() method in wrapper classes like Boolean, Integer etc.
-* 
-* 
-* 
+* <h3>Factory Method Design Pattern examples in JDK</h3>
+* <ul>
+*	<li>java.util.Calendar, ResourceBundle and NumberFormat getInstance() methods uses Factory pattern.</li>
+*   <li>valueOf() method in wrapper classes like Boolean, Integer etc.</li>
+* </ul>
 * 
 * @author Ankur Maurya
 * 
