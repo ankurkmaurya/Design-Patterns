da109737b776463143544ba82e8c88658b957609
Ankur Maurya <100197611+ankurkmaurya@users.noreply.github.com>
2022-10-31 13:05:31 +0530

    Structural Design Pattern added
    Project for Structural Design Pattern added

---
+++ Structural Patterns/StructuralPatterns/src/main/java/com/ankurmaurya/structural/Structural.java
@@ -0,0 +1,43 @@
+package com.ankurmaurya.structural;
+
+/**
+ * 
+ * <h1>Structural Design Patterns</h1>
+ * 
+ * 
+ * <h3>Points :</h3>
+ * <ul>
+ * <li>Structural patterns provide different ways to create a class structure,
+ * for example using inheritance and composition to create a large object from
+ * small objects.</li>
+ * <li>These design patterns are all about Class and Object composition.
+ * Structural class-creation patterns use inheritance to compose
+ * interfaces.</li>
+ * <li>Structural object-patterns define ways to compose objects to obtain new
+ * functionality.</li>
+ * </ul>
+ * 
+ * <h3>Various Structural Design Patterns</h3>
+ * <ol>
+ * <li><b>Adapter : </b>Match interfaces of different classes</li>
+ * <li><b>Bridge : </b>Separates an object’s interface from its implementation</li>
+ * <li><b>Composite : </b>A tree structure of simple and composite objects</li>
+ * <li><b>Decorator : </b>Add responsibilities to objects dynamically</li>
+ * <li><b>Facade : </b>A single class that represents an entire subsystem</li>
+ * <li><b>Flyweight : </b>A fine-grained instance used for efficient sharing</li>
+ * <li><b>Proxy : </b>An object representing another object</li>
+ * <li><b>Private Class Data : </b>Restricts accessor/mutator access</li>
+ * </ol>
+ * 
+ * 
+ * @author Ankur Maurya
+ * 
+ */
+
+
+public class Structural {
+
+	public static void main(String[] args) {
+		System.out.println("------ STRUCTURAL DESIGN PATTERNS ------");
+	}
+}
---
+++ Structural Patterns/StructuralPatterns/src/main/java/com/ankurmaurya/structural/adapter/Adapter.java
@@ -0,0 +1,33 @@
+package com.ankurmaurya.structural.adapter;
+
+
+/**
+ * 
+ * <h1>Adapter Design Pattern</h1>
+ * 
+ * 
+ * <h3>Points :</h3>
+ * <ul>
+ * <li>Structural patterns provide different ways to create a class structure,
+ * for example using inheritance and composition to create a large object from
+ * small objects.</li>
+ * <li>These design patterns are all about Class and Object composition.
+ * Structural class-creation patterns use inheritance to compose
+ * interfaces.</li>
+ * <li>Structural object-patterns define ways to compose objects to obtain new
+ * functionality.</li>
+ * </ul>
+ * 
+ * 
+ * 
+ * @author Ankur Maurya
+ * 
+ */
+
+
+public class Adapter {
+
+	
+	
+	
+}
