317c97f933f8ece271b667ecd51cdee218f2125d
Ankur Maurya <100197611+ankurkmaurya@users.noreply.github.com>
2022-11-17 17:02:53 +0530

    Memento Updated

--- Behavioral Patterns/BehavioralPatterns/src/main/java/com/ankurmaurya/behavioral/memento/Memento.java
+++ Behavioral Patterns/BehavioralPatterns/src/main/java/com/ankurmaurya/behavioral/memento/Memento.java
@@ -4,8 +4,17 @@ package com.ankurmaurya.behavioral.memento;
  * 
  * <h3>Memento Design Pattern</h3>
  * <p>
+ * The memento design pattern is used when we want to save the state of an
+ * object so that we can restore later on. Memento pattern is used to implement
+ * this in such a way that the saved state data of the object is not accessible
+ * outside of the object, this protects the integrity of saved state data.
  * </p>
  * <p>
+ * Memento pattern is implemented with two objects – <i>Originator</i> and
+ * <i>Caretaker</i>. The originator is the object whose state needs to be saved
+ * and restored and it uses an inner class to save the state of Object. The
+ * inner class is called Memento and it’s private so that it can’t be accessed
+ * from other objects.
  * </p>
  * 
  * 
@@ -37,13 +46,16 @@ package com.ankurmaurya.behavioral.memento;
  * 
  */
 
-
 public class Memento {
 
-	public static void main(String[] args) {
 	
+	public static void main(String[] args) {
+
+		
 		
 		
 	}
 	
+	
+
 }
